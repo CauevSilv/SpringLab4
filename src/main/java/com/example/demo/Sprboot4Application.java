5bd6a57ea2c34d28a150e6fde88823833cef5721
CauevSilv <caue.silva10@fatec.sp.gov.br>
2024-08-13 19:52:38 -0300

    First

---
+++ src/main/java/com/example/demo/Sprboot4Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprboot4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprboot4Application.class, args);
+	}
+
+}
